bca197d0f502bd6583f1b0237b1fe76b882895e9
SG Jang <wkdtjrrms0@gmail.com>
2021-12-17 00:08:33 +0900

    Change: Change PS(dp)
    BOJ(#1463)

--- BOJ/dp/#1463.java
+++ BOJ/dp/#1463.java
@@ -1,4 +1,29 @@
+//Bottom-Up
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] D = new int[num + 1];
+        D[1] = 0;
+        for(int i = 2; i <= num; i++){
+            D[i] = D[i - 1] + 1;
+            if(i % 2 == 0 && D[i] > D[i / 2] + 1){
+                D[i] = D[i / 2] + 1;
+            }
+            if(i % 3 == 0 && D[i] > D[i / 3] + 1){
+                D[i] = D[i / 3] + 1;
+            }
+        }
+        bw.write(Integer.toString(D[num]));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
 
 /* Top-Down
 import java.io.*;
