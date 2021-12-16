b1e1f92e0d933c0801d9ada53577a9ce14138709
SG Jang <wkdtjrrms0@gmail.com>
2021-12-17 01:07:27 +0900

    Resolve: Change PS(dp)
    BOJ(#10844)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+/* Bottom-Up
 import java.io.*;
 
 public class Main {
@@ -33,3 +34,50 @@ public class Main {
         br.close();
     }
 }
+*/
+
+// Top-Down
+import java.io.*;
+
+public class Main {
+    static Long[][] D;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        D = new Long[num + 1][10];
+        for(int i = 0; i < 10; i++) {
+            D[1][i] = 1L;
+        }
+
+        long ans = 0;
+        for(int i = 1; i < 10; i++) {
+            ans += DP(num, i);
+        }
+        bw.write(String.valueOf(ans % 1000000000));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static long DP(int n, int val){
+        if(n == 1){
+            return D[n][val];
+        }
+        if(D[n][val] == null){
+            if(val == 0){
+                D[n][val] = DP(n - 1, 1);
+            }
+            else if(val == 9){
+                D[n][val] = DP(n - 1, 8);
+            }
+            else{
+                D[n][val] = DP(n - 1, val - 1) + DP(n - 1, val + 1);
+            }
+        }
+        return D[n][val] % 1000000000;
+    }
+}
+//
