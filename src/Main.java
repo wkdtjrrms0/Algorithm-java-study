1ecc9eebe245127402645dcbbff9469c1c3dcc81
SG Jang <wkdtjrrms0@gmail.com>
2021-12-24 22:15:48 +0900

    Answer: Add PS(etc)
    BOJ(#15829)

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,20 @@
-// Bottom-Up
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final int r=31;
+    static final int M=1234567891;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int[] DP = new int[n + 1];
-        int[] Val = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for (int i = 0; i < n; i++) {
-            Val[i + 1] = Integer.parseInt(st.nextToken());
+
+        int L=Integer.parseInt(br.readLine());
+        String s=br.readLine();
+        long answer=0;
+        for(int i=0;i<L;i++){
+            answer+=(s.charAt(i)-'a'+1)*pow(r,i);
         }
-        for(int i = 1; i <= n; i++){
-            DP[i] = 1;
-            for(int j = 1; j <= i; j++){
-                if(Val[j] > Val[i] && DP[i] < DP[j] + 1) {
-                    DP[i] = DP[j] + 1;
-                }
-            }
-        }
-        int max = -1;
-        for(int i = 1; i <= n; i++){
-            if(DP[i] > max){
-                max = DP[i];
-            }
-        }
-        bw.write(Integer.toString(max));
-        bw.flush();
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(answer%M);
+    }
+    static long pow(int a,int b){
+        return b==0?1:a*pow(a,b-1)%M;
     }
 }
-//
-
-/* Top-Down
-import java.io.*;
-import java.util.StringTokenizer;
-
-public class Main {
-    static Integer[] DP;
-    static int[] Val;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        DP = new Integer[n + 1];
-        Val = new int[n + 1];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for (int i = 0; i < n; i++) {
-            Val[i + 1] = Integer.parseInt(st.nextToken());
-        }
-        for(int i = 1; i <= n; i++) {
-            result(i);
-        }
-
-        int max = -1;
-        for(int i = 1; i <= n; i++){
-            if(DP[i] > max){
-                max = DP[i];
-            }
-        }
-        bw.write(Integer.toString(max));
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-    static int result(int n){
-        if(DP[n] == null) {
-            DP[n] = 1;
-            for (int i = n - 1; i >= 1; i--) {
-                if (Val[i] > Val[n]) {
-                    DP[n] = Math.max(DP[n], result(i) + 1);
-                }
-            }
-        }
-        return DP[n];
-    }
-}
-*/
