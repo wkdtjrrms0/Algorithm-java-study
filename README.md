bf30d4406c3c037ec060350d919c6d60e6c61ffe
SG Jang <wkdtjrrms0@gmail.com>
2022-01-28 19:11:31 +0900

    Change: Change Readme.md
    Change Readme.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Algorithm-java-study
-1일 1커밋을 목표로 알고리즘을 공부하는 리포지토리입니다.<br> 알고리즘 사이트로는 BOJ, Programmers, Leetcode를 사용합니다.<br>
+알고리즘을 공부하는 리포지토리입니다.<br> 알고리즘 사이트로는 BOJ, Programmers, Leetcode를 사용합니다.<br>
 사이트별 디렉토리는 [수학, 구현, 그리디, 문자열, 자료구조, 그래프, DP, 기하학, 기타]으로 구성됩니다.
 
 ## User INFO
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,41 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        long count5 = five_power(n) - five_power(n - m) - five_power(m);
-        long count2 = two_power(n) - two_power(n - m) - two_power(m);
-        bw.write(String.valueOf(Math.min(count2, count5)));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b){
+                if(a[0] == b[0]){
+                    return a[1] - b[1];
+                }
+                else{
+                    return a[0] - b[0];
+                }
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i< N ; i++) {
+            sb.append(arr[i][1] + " " + arr[i][0]).append('\n');
+        }
+        bw.write(String.valueOf(sb));
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    static long five_power(int n){
-        int count = 0;
-        while(n >= 5){
-            count += n/5;
-            n /= 5;
-        }
-        return count;
-    }
-    static long two_power(int n){
-        int count = 0;
-        while(n >= 2){
-            count += n/2;
-            n /= 2;
-        }
-        return count;
     }
 }
