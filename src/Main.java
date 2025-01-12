26b228f76bd3abae0fcda48157320364618c1056
SG Jang <wkdtjrrms0@gmail.com>
2025-01-12 19:47:10 +0900

    Solve: Add PS

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Info: Programmers#42586 기능개발
@@ -10,26 +12,39 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		// case 1
-//		int[] arr = {1, 1, 3, 3, 0, 1, 1};
-		// case 2
-		int[] arr = {4, 4, 4, 3, 3};
-		
-		System.out.println(Arrays.toString(solution(arr)));
-	}
-	
-	public static int[] solution(int []arr) {
-		Deque<Integer> q = new ArrayDeque<>();
-		
-		for (int i = 0; i < arr.length; i++) {
-			if (q.size() == 0 || q.peekLast() != arr[i]) {
-				q.offer(arr[i]);
-			}
-		}
-		
-		int[] answer = q.stream()
-				.mapToInt(t -> t)
-				.toArray();
+//		int[] progresses = {93, 30, 55};
+//		int[] speeds = {1, 30, 5};
 
-        return answer;
-    }
+		// case 2
+		int[] progresses = {95, 90, 99, 99, 80, 99};
+		int[] speeds = {1, 1, 1, 1, 1, 1};
+		
+		System.out.println(Arrays.toString(solution(progresses, speeds)));
+	}
+
+	public static int[] solution(int[] progresses, int[] speeds) {
+
+		Deque<Integer> q = new ArrayDeque<>();
+
+		for (int i = 0; i < progresses.length; i++) {
+			int remainingDays = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+			q.offer(remainingDays);
+		}
+
+		List<Integer> deployments = new ArrayList<>();
+
+		while (!q.isEmpty()) {
+			int current = q.poll();
+			int count = 1;
+
+			while (!q.isEmpty() && q.peek() <= current) {
+				q.poll();
+				count++;
+			}
+
+			deployments.add(count);
+		}
+
+		return deployments.stream().mapToInt(i -> i).toArray();
+	}
 }
