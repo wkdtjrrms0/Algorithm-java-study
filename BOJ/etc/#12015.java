39d7b1992a7c2dd623dc4bc7111cb23a6063f176
SG Jang <wkdtjrrms0@gmail.com>
2025-04-28 01:40:34 +0900

    Solve: Add PS

---
+++ BOJ/etc/#12015.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Info: BOJ#12015 가장 긴 증가하는 부분 수열 2
+ * Ref: https://www.acmicpc.net/problem/12015
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        System.out.println(solution(A));
+    }
+
+    public static int solution(int[] A) {
+        int[] LIS = new int[A.length];
+        LIS[0] = A[0];
+        int answer = 1;
+
+        for (int i = 1; i < A.length; i++) {
+            int num = A[i];
+            if (num > LIS[answer - 1]) {
+                answer++;
+                LIS[answer - 1] = num;
+            } else {
+                int left = 0;
+                int right = answer - 1;
+                int ans = 0;
+
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (LIS[mid] >= num) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                        ans = left;
+                    }
+                }
+                LIS[ans] = num;
+            }
+        }
+        return answer;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,45 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Info: BOJ#1300 K번째 수
- * Ref: https://www.acmicpc.net/problem/1300
+ * Info: BOJ#12738 가장 긴 증가하는 부분 수열 3
+ * Ref: https://www.acmicpc.net/problem/12738
  */
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		long k = sc.nextInt();
-		System.out.println(solution(N, k));
+		int[] A = new int[N];
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		System.out.println(solution(A));
 	}
+	public static int solution(int[] A) {
+		int[] LIS = new int[A.length];
+		LIS[0] = A[0];
+		int answer = 1;
 
-	public static long solution(int N, long k) {
-		long left = 1;
-		long right = k;
-		long answer = 0;
-
-		while (left <= right) {
-			long count = 0;
-			long mid = (left + right) / 2;
-			for (int i = 1; i <= N; i++) {
-				count += Math.min(mid / i, N);
-			}
-
-			if (count < k) {
-				left = mid + 1;
+		for (int i = 1; i < A.length; i++) {
+			int num = A[i];
+			if (num > LIS[answer - 1]) {
+				answer++;
+				LIS[answer - 1] = num;
 			} else {
-				right = mid - 1;
-				answer = mid;
+				int left = 0;
+				int right = answer - 1;
+				int ans = 0;
+
+				while (left < right) {
+					int mid = (left + right) / 2;
+					if (LIS[mid] >= num) {
+						right = mid;
+					} else {
+						left = mid + 1;
+						ans = left;
+					}
+				}
+				LIS[ans] = num;
 			}
 		}
 		return answer;
