8e605e37b7e169aea7071d8c41d38bf0fec62443
SG Jang <wkdtjrrms0@gmail.com>
2025-08-17 22:11:50 +0900

    Solve: Add PS

--- BOJ/etc/#15810.java
+++ BOJ/etc/#15810.java
@@ -5,36 +5,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- * Info: BOJ#17245 서버실
- * Ref: https://www.acmicpc.net/problem/17245
+ * Info: BOJ#15810 풍선 공장
+ * Ref: https://www.acmicpc.net/problem/15810
  */
 public class Main {
 
     static int N;
-    static long[][] rack;
-    static long pcSum = 0;
-    static long max = Long.MIN_VALUE;
+    static long M;
+    static long[] A;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        rack = new long[N][N];
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+
+        A = new long[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                long k = Long.parseLong(st.nextToken());
-                rack[i][j] = k;
-                pcSum += k;
-                max = Math.max(max, k);
-            }
+            A[i] = Long.parseLong(st.nextToken());
         }
+        Arrays.sort(A);
         System.out.println(solution());
     }
 
     private static long solution() {
         long left = 1;
-        long right = max;
+        long right = A[0] * M;
         long answer = 0;
 
         while (left <= right) {
@@ -52,16 +49,10 @@ public class Main {
 
     private static boolean isOK(long mid) {
         long cnt = 0;
-        for (long[] i : rack) {
-            for (long j : i) {
-                if (j <= mid) {
-                    cnt += j;
-                } else {
-                    cnt += mid;
-                }
-            }
+        for (long a : A) {
+            cnt += mid / a;
+            if (cnt >= M) return true;
         }
-        double activePCRate = ((double) cnt / pcSum) * 100;
-        return activePCRate >= 50;
+        return false;
     }
 }
